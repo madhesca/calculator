5f7b4f0019a7ddca653a78f8bcb86fed4dff3595
marton <mpochavillo@gmail.com>
2020-06-09 15:19:47 +0800

    done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import ExpenseForm from "./components/ExpenseForm";
 import Alert from "./components/Alert";
 import ExpenseList from "./components/ExpenseList";
 
-const initialExpenses = [
-  { id: 1, charge: "rent", amount: 1600 },
-  { id: 2, charge: "car payment", amount: 400 },
-  { id: 3, charge: "credit card bill", amount: 1200 }
-];
+// const initialExpenses = [
+//   { id: 1, charge: "rent", amount: 1600 },
+//   { id: 2, charge: "car payment", amount: 400 },
+//   { id: 3, charge: "credit card bill", amount: 1200 },
+// ];
+
+const initialExpenses = localStorage.getItem("expenses")
+  ? JSON.parse(localStorage.getItem("expenses"))
+  : [];
 
 function App() {
   const [expenses, setExpenses] = useState(initialExpenses);
@@ -17,11 +21,16 @@ function App() {
   const [alert, setAlert] = useState({
     show: false,
     type: "amount",
-    text: ""
+    text: "",
   });
 
   const [edit, setEdit] = useState(false);
   const [id, setId] = useState(0);
+
+  useEffect(() => {
+    console.log("useEffect called");
+    localStorage.setItem("expenses", JSON.stringify(expenses));
+  }, [expenses]);
 
   const clearItems = () => {
     console.log("All Items are cleared");
@@ -29,8 +38,8 @@ function App() {
     handleAlert({ type: "success", text: "All items were deleted" });
   };
 
-  const handleEdit = id => {
-    const expenseEdit = expenses.find(item => item.id === id);
+  const handleEdit = (id) => {
+    const expenseEdit = expenses.find((item) => item.id === id);
     const { charge, amount } = expenseEdit;
     setAmount(amount);
     setCharge(charge);
@@ -38,23 +47,21 @@ function App() {
     setId(id);
   };
 
-  const handleDelete = id => {
+  const handleDelete = (id) => {
     console.log("Item Deleted", id);
-    const singleDelete = expenses.filter(expense => {
+    const singleDelete = expenses.filter((expense) => {
       return expense.id !== id;
     });
     setExpenses(singleDelete);
     handleAlert({ type: "success", text: "succesfully deleted the item" });
   };
 
-  const handleCharge = e => {
+  const handleCharge = (e) => {
     setCharge(e.target.value);
-    console.log("charge", e.target.value);
   };
 
-  const handleAmount = e => {
+  const handleAmount = (e) => {
     setAmount(e.target.value);
-    console.log("amount", e.target.value);
   };
 
   const handleAlert = ({ type, text }) => {
@@ -64,33 +71,53 @@ function App() {
     }, 3000);
   };
 
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
 
     if (charge !== "" && amount > 0) {
-      const singleExpense = { id: Math.random(), charge, amount };
-      setExpenses([...expenses, singleExpense]);
+      if (edit) {
+        let tempExpense = expenses.map((item) => {
+          return item.id === id ? { ...item, charge, amount } : item;
+        });
+
+        setExpenses(tempExpense);
+        setEdit(false);
+        handleAlert({ type: "success", text: "Successfully Edited expense" });
+      } else {
+        const singleExpense = { id: Math.random(), charge, amount };
+        setExpenses([...expenses, singleExpense]);
+        handleAlert({ type: "success", text: "Successfully Added expense" });
+      }
     } else {
       return handleAlert({
         type: "danger",
-        text: "Fields should not be emptied"
+        text: "Fields should not be emptied",
       });
     }
 
-    handleAlert({ type: "success", text: "Successfully added expense" });
     setCharge("");
     setAmount("");
   };
-
-  console.log(expenses);
 
   return (
     <>
       {alert.show && <Alert type={alert.type} text={alert.text} />}
       <h1>Budget Calculator</h1>
       <main className="App">
-        <ExpenseForm charge={charge} amount={amount} handleAmount={handleAmount} handleCharge={handleCharge} handleSubmit={handleSubmit} edit={edit} />
-        <ExpenseList expenses={expenses} handleDelete={handleDelete} handleEdit={handleEdit} clearItems={clearItems} />
+        <ExpenseForm
+          charge={charge}
+          amount={amount}
+          handleAmount={handleAmount}
+          handleCharge={handleCharge}
+          handleSubmit={handleSubmit}
+          edit={edit}
+        />
+        <ExpenseList
+          expenses={expenses}
+          handleDelete={handleDelete}
+          handleEdit={handleEdit}
+          clearItems={clearItems}
+        />
       </main>
       <h1>
         Total Spending:
